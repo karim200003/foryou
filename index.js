21e3214025707d9895c2c41fe8279cdf919789f0
karim200003 <94787118+karim200003@users.noreply.github.com>
2024-03-07 19:04:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("click",t7wa);
+function t7wa(){
+    document.querySelector("button").textContent = "sir tm7wa";
+    var chrr = new Audio("./videoplayback.m4a");
+    chrr.play();
+}
